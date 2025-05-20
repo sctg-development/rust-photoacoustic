7b93cbf329a3fd5415a286f9ac2571638b3ffa85
Ronan <ronan.le_meillat@highcanfly.club>
2025-05-20 08:29:54 +0200

    feat: add modbus skeleton

---
+++ src/modbus/modbus_server.rs
@@ -0,0 +1,124 @@
+// Copyright (c) 2025 Ronan LE MEILLAT, SCTG Development
+// This file is part of the rust-photoacoustic project and is licensed under the
+// SCTG Development Non-Commercial License v1.0 (see LICENSE.md for details).
+
+//! Modbus server implementation for the photoacoustic water vapor analyzer
+//! 
+//! For avoiding confusion with the Modbus master/slave terminology, this module uses
+//! the terms "server" and "client" instead. The server is the device that provides data,
+//! while the client is the device that requests data.
+//! The modbus master is the device that requests data, while the modbus slave is the device that provides data.
+//! In other words the modbus master is here the client and the modbus slave is here the server.
+
+use std::{
+    collections::HashMap,
+    future,
+    net::SocketAddr,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
+
+use tokio::net::TcpListener;
+
+use tokio_modbus::{
+    prelude::*,
+    server::tcp::{accept_tcp_connection, Server},
+};
+
+struct PhotoacousticModbusServer {
+    input_registers: Arc<Mutex<HashMap<u16, u16>>>,
+    holding_registers: Arc<Mutex<HashMap<u16, u16>>>,
+}
+
+impl tokio_modbus::server::Service for PhotoacousticModbusServer {
+    type Request = Request<'static>;
+    type Response = Response;
+    type Exception = ExceptionCode;
+    type Future = future::Ready<Result<Self::Response, Self::Exception>>;
+
+    fn call(&self, req: Self::Request) -> Self::Future {
+        let res = match req {
+            Request::ReadInputRegisters(addr, cnt) => {
+                register_read(&self.input_registers.lock().unwrap(), addr, cnt)
+                    .map(Response::ReadInputRegisters)
+            }
+            Request::ReadHoldingRegisters(addr, cnt) => {
+                register_read(&self.holding_registers.lock().unwrap(), addr, cnt)
+                    .map(Response::ReadHoldingRegisters)
+            }
+            Request::WriteMultipleRegisters(addr, values) => {
+                register_write(&mut self.holding_registers.lock().unwrap(), addr, &values)
+                    .map(|_| Response::WriteMultipleRegisters(addr, values.len() as u16))
+            }
+            Request::WriteSingleRegister(addr, value) => register_write(
+                &mut self.holding_registers.lock().unwrap(),
+                addr,
+                std::slice::from_ref(&value),
+            )
+            .map(|_| Response::WriteSingleRegister(addr, value)),
+            _ => {
+                error!("SERVER: Exception::IllegalFunction - Unimplemented function code in request: {req:?}");
+                Err(ExceptionCode::IllegalFunction)
+            }
+        };
+        future::ready(res)
+    }
+}
+
+impl PhotoacousticModbusServer {
+    fn new() -> Self {
+        // Insert some test data as register values.
+        let mut input_registers = HashMap::new();
+        input_registers.insert(0, 1234);
+        input_registers.insert(1, 5678);
+        let mut holding_registers = HashMap::new();
+        holding_registers.insert(0, 10);
+        holding_registers.insert(1, 20);
+        holding_registers.insert(2, 30);
+        holding_registers.insert(3, 40);
+        Self {
+            input_registers: Arc::new(Mutex::new(input_registers)),
+            holding_registers: Arc::new(Mutex::new(holding_registers)),
+        }
+    }
+}
+
+/// Helper function implementing reading registers from a HashMap.
+fn register_read(
+    registers: &HashMap<u16, u16>,
+    addr: u16,
+    cnt: u16,
+) -> Result<Vec<u16>, ExceptionCode> {
+    let mut response_values = vec![0; cnt.into()];
+    for i in 0..cnt {
+        let reg_addr = addr + i;
+        if let Some(r) = registers.get(&reg_addr) {
+            response_values[i as usize] = *r;
+        } else {
+            error!("SERVER: Exception::IllegalDataAddress");
+            return Err(ExceptionCode::IllegalDataAddress);
+        }
+    }
+
+    Ok(response_values)
+}
+
+/// Write a holding register. Used by both the write single register
+/// and write multiple registers requests.
+fn register_write(
+    registers: &mut HashMap<u16, u16>,
+    addr: u16,
+    values: &[u16],
+) -> Result<(), ExceptionCode> {
+    for (i, value) in values.iter().enumerate() {
+        let reg_addr = addr + i as u16;
+        if let Some(r) = registers.get_mut(&reg_addr) {
+            *r = *value;
+        } else {
+            error!("SERVER: Exception::IllegalDataAddress");
+            return Err(ExceptionCode::IllegalDataAddress);
+        }
+    }
+
+    Ok(())
+}
