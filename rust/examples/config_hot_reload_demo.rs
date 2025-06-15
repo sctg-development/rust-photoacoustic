78ec7ffa73879f0720028ec39da92fa797f5427d
Ronan L <aeltorio@universidadatlantico.org>
2025-06-15 16:27:20 +0200

    doc: misssing header

--- rust/examples/config_hot_reload_demo.rs
+++ rust/examples/config_hot_reload_demo.rs
@@ -1,3 +1,7 @@
+// Copyright (c) 2025 Ronan LE MEILLAT, SCTG Development
+// This file is part of the rust-photoacoustic project and is licensed under the
+// SCTG Development Non-Commercial License v1.0 (see LICENSE.md for details).
+
 //! Manual test example for dynamic configuration hot-reload
 //!
 //! This example demonstrates how the ProcessingConsumer automatically detects
--- rust/tests/integration_config_hot_reload.rs
+++ rust/tests/integration_config_hot_reload.rs
@@ -1,3 +1,7 @@
+// Copyright (c) 2025 Ronan LE MEILLAT, SCTG Development
+// This file is part of the rust-photoacoustic project and is licensed under the
+// SCTG Development Non-Commercial License v1.0 (see LICENSE.md for details).
+
 //! Integration test for dynamic configuration hot-reload feature
 //!
 //! This test verifies that the ProcessingConsumer can detect configuration changes
