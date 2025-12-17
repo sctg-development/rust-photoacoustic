fdd5b5c28a521eab50f1409117a5609e9e670b96
Ronan L <aeltorio@universidadatlantico.org>
2025-12-17 08:29:14 +0100

    add copytight

--- rust/docker/remove-auto-init/src/main.rs
+++ rust/docker/remove-auto-init/src/main.rs
@@ -1,3 +1,9 @@
+// Copyright (c) 2025 Ronan LE MEILLAT, SCTG Development
+// This file is part of the rust-photoacoustic project and is licensed under the
+// SCTG Development Non-Commercial License v1.0 (see LICENSE.md for details).
+// Tool to remove "auto-initialize" feature from pyo3 dependencies in Cargo.toml
+// auto-initialize feature should not be used while using a static libpython build
+
 use std::env;
 use std::fs;
 use std::process;
